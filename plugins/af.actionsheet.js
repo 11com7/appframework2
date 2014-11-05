fb17ad37501a63d428c8b1fa2adff534dfdc4dbf
Dominik Pesch <d.pesch@11com7.de>
2014-11-05 12:21:43 +0100

    fixed (again): height error on android

--- plugins/af.actionsheet.js
+++ plugins/af.actionsheet.js
@@ -105,8 +105,9 @@
             this.activeSheet = markup;
             $(elID).append("<div id='af_action_mask' style='position:absolute;top:0px;left:0px;right:0px;bottom:0px;z-index:9998;background:rgba(0,0,0,.4)'/>");
             setTimeout(function() {
+                var height = markup.height();
                 markup.vendorCss("Transition", "all 300ms");
-                markup.cssTranslate("0," + (-(markup.height())) + "px");
+                markup.cssTranslate("0," + (-height) + "px");
             }, 10);
             $("#af_action_mask").bind("touchstart touchmove touchend click",function(e){
                 e.preventDefault();
